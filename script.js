8486b94cd25f16ad1e274b1781a78fa79a478c72
astucioso <150879600+astucioso@users.noreply.github.com>
2024-12-16 03:16:19 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
-// script.js
 
 document.getElementById('revealButton').addEventListener('click', function() {
-    // Iniciar a música
     var audio = document.getElementById('background-music');
-    audio.play();  // Inicia a reprodução da música
+    audio.play();
 
-    // Esconde a tela inicial (botão e texto)
     document.querySelector('.start-screen').style.display = 'none';
 
-    // Exibe os perfis
     document.getElementById('profiles-container').style.display = 'flex';
 });
