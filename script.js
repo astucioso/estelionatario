e12db5e184b328d2b3abc613076dabe0452f3fb5
astucioso <150879600+astucioso@users.noreply.github.com>
2024-12-16 11:24:35 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,29 +6,27 @@ function updateProfileImage(userId) {
       const profileImage = document.querySelector('.profile-img[data-user-id="' + userId + '"]');
       const usernameElement = document.querySelector('.nickr[data-user-id="' + userId + '"]');
       const subnickElement = document.querySelector('.subnick[data-user-id="' + userId + '"]');
-      const badgesContainer = document.querySelector('.badges[badges-user-id="' + userId + '"]');
+      const badgesContainer = document.getElementById('badges-' + userId);  // Alteração aqui: uso de id para badges
       
       profileImage.src = user.avatar
         ? 'https://cdn.discordapp.com/avatars/' + user.id + '/' + user.avatar + '.' + (user.avatar.startsWith('a_') ? 'gif' : 'png') + '?size=512'
         : 'https://cdn.discordapp.com/embed/avatars/1.png';
   
       usernameElement.textContent = user.display_name ? user.display_name : user.username;
-  
       subnickElement.textContent = user.username;
   
-      badgesContainer.innerHTML = '';
-      for (let badge of user.public_flags.badges) {
-        let badgeElement = document.createElement('div');
-        badgeElement.className = 'badge';
-        badgeElement.style.backgroundImage = badge.id === 'premium' 
-          ? "url('https://raw.githubusercontent.com/Rep7/badges/main/svg/discordnitro.svg')" 
-          : "url('https://raw.githubusercontent.com/mezotv/discord-badges/" + badge.id + ".svg')";
-        badgeElement.dataset.tooltip = badge.description;
-        badgesContainer.appendChild(badgeElement);
+      badgesContainer.innerHTML = ''; // Limpar qualquer conteúdo existente
+      if (user.public_flags && user.public_flags.badges) {
+        for (let badge of user.public_flags.badges) {
+          let badgeElement = document.createElement('div');
+          badgeElement.className = 'badge';
+          badgeElement.style.backgroundImage = badge.id === 'premium' 
+            ? "url('https://raw.githubusercontent.com/Rep7/badges/main/svg/discordnitro.svg')" 
+            : "url('https://raw.githubusercontent.com/mezotv/discord-badges/" + badge.id + ".svg')";
+          badgeElement.dataset.tooltip = badge.description;
+          badgesContainer.appendChild(badgeElement);
+        }
       }
-
-      const subnickContainer = subnickElement.parentElement;
-      subnickContainer.appendChild(badgesContainer);
     })
     .catch(error => {
       console.error('Error fetching profile data:', error);
